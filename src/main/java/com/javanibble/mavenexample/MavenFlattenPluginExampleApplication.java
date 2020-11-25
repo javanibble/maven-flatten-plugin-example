c1b6c1197ba349a9a7309d1d1922542c2f3ad395
javanibble <javanibble@gmail.com>
2020-11-25 11:50:49 +0100

    Add SpringBoot Application

---
+++ src/main/java/com/javanibble/mavenexample/MavenFlattenPluginExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javanibble.mavenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenFlattenPluginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenFlattenPluginExampleApplication.class, args);
+	}
+
+}
